86806617ea5bcbe29825abe59d63b26e31425e1a
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-20 13:55:56 +0200

    changes reducer state before api call

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 	const handleButtonAdd = () => {
 		window.scrollTo({
 			top: 0,
-			behavior: "smooth"
+			behavior: 'smooth',
 		});
 	};
 	return (
@@ -35,29 +35,179 @@ function App() {
 			<h2>Items from API</h2>
 
 			<div className="germanNounArea">
-				{state.germanNouns.map((gn, i) => {
+				{state.germanNouns.map((item, i) => {
 					return (
 						<fieldset className="germanNoun" key={i}>
-							<legend>ID: {gn.id}</legend>
+							<legend>ID: {item.id}</legend>
 
 							<div className="row">
 								<label htmlFor="article">Article</label>
-								<div className="value">{gn.article}</div>
+								{item.isEditing ? (
+									<input
+										id="article"
+										value={item.article}
+										onChange={(e) =>
+											dispatch({
+												type: 'changeItemProperty',
+												payload: {
+													itemType: 'germanNouns',
+													property: 'article',
+													id: item.id,
+													value: e.target.value,
+												},
+											})
+										}
+									/>
+								) : (
+									<div className="value">{item.article}</div>
+								)}
 							</div>
 
 							<div className="row">
 								<label htmlFor="singular">Singular</label>
-								<div className="value">{gn.singular}</div>
+								{item.isEditing ? (
+									<input
+										id="singular"
+										value={item.singular}
+										onChange={(e) =>
+											dispatch({
+												type: 'changeItemProperty',
+												payload: {
+													itemType: 'germanNouns',
+													property: 'singular',
+													id: item.id,
+													value: e.target.value,
+												},
+											})
+										}
+									/>
+								) : (
+									<div className="value">{item.singular}</div>
+								)}
 							</div>
 
 							<div className="row">
 								<label htmlFor="plural">Plural</label>
-								<div className="value">{gn.plural}</div>
+								{item.isEditing ? (
+									<input
+										id="plural"
+										value={item.plural}
+										onChange={(e) =>
+											dispatch({
+												type: 'changeItemProperty',
+												payload: {
+													itemType: 'germanNouns',
+													property: 'plural',
+													id: item.id,
+													value: e.target.value,
+												},
+											})
+										}
+									/>
+								) : (
+									<div className="value">{item.plural}</div>
+								)}
 							</div>
 							<div className="buttonRow">
-								<button>Edit</button>
-								<button>Delete</button>
-								<button onClick={handleButtonAdd}>Add</button>
+								<div className="manageMessage">
+									{item.manageMessage}
+								</div>
+								<div className="buttonArea">
+									{!item.isEditing && !item.isDeleting && (
+										<>
+											<button
+												onClick={(e) =>
+													dispatch({
+														type: 'toggleItemEditing',
+														payload: {
+															itemType:
+																'germanNouns',
+															id: item.id,
+														},
+													})
+												}
+											>
+												Edit
+											</button>
+											<button
+												onClick={(e) =>
+													dispatch({
+														type: 'toggleItemDeleting',
+														payload: {
+															itemType:
+																'germanNouns',
+															id: item.id,
+														},
+													})
+												}
+											>
+												Delete
+											</button>
+											<button onClick={handleButtonAdd}>
+												Add
+											</button>
+										</>
+									)}
+									{item.isEditing && (
+										<>
+											<button
+												onClick={(e) =>
+													dispatch({
+														type: 'clearItemEditing',
+														payload: {
+															itemType:
+																'germanNouns',
+															id: item.id,
+														},
+													})
+												}
+											>Clear</button>
+											<button
+												onClick={(e) =>
+													dispatch({
+														type: 'saveItemEditing',
+														payload: {
+															itemType:
+																'germanNouns',
+															id: item.id,
+														},
+													})
+												}>
+											Save	
+											</button>
+										</>
+									)}
+									{item.isDeleting && (
+										<>
+											<button
+												onClick={(e) =>
+													dispatch({
+														type: 'clearItemEditing',
+														payload: {
+															itemType:
+																'germanNouns',
+															id: item.id,
+														},
+													})
+												}
+											>Yes, delete it.</button>
+											<button
+												
+												onClick={(e) =>
+													dispatch({
+														type: 'clearItemDeleting',
+														payload: {
+															itemType:
+																'germanNouns',
+															id: item.id,
+														},
+													})
+												}>
+											No, do not delete it.	
+											</button>
+										</>
+									)}
+								</div>
 							</div>
 						</fieldset>
 					);
--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -9,10 +9,22 @@ const api_base_url = 'http://localhost:4555';
 
 const initialState = {
 	count: 0,
-	germanNouns: []
+	germanNouns: [],
+	germanNounForm: {
+		article: 'aa',
+		singular: 'ss',
+		plural: 'pp',
+	},
 };
 
 function reducer(state, action) {
+	let itemType = '';
+	let property = '';
+	let id = 0;
+	let value = '';
+	let item = {};
+	let saveItem = {};
+
 	let obj = { ...state };
 	switch (action.type) {
 		case 'increaseCount':
@@ -24,6 +36,54 @@ function reducer(state, action) {
 		case 'loadGermanNouns':
 			obj.germanNouns = action.payload;
 			break;
+		case 'changeItemProperty':
+			itemType = action.payload.itemType;
+			property = action.payload.property;
+			id = action.payload.id;
+			value = action.payload.value;
+			state[itemType].find((m) => m.id === id)[property] = value;
+			break;
+		case 'toggleItemEditing':
+			itemType = action.payload.itemType;
+			id = action.payload.id;
+			item = state[itemType].find((m) => m.id === id);
+			item.isEditing = !item.isEditing;
+			item.manageMessage = 'Edit this item.'
+			break;
+		case 'clearItemEditing':
+			itemType = action.payload.itemType;
+			id = action.payload.id;
+			item = state[itemType].find((m) => m.id === id);
+			item.isEditing = false; 
+			item.article = item.originalItem.article;
+			item.singular = item.originalItem.singular;
+			item.plural = item.originalItem.plural;
+			item.manageMessage = 'Manage options:';
+			break;
+		case 'toggleItemDeleting':
+			itemType = action.payload.itemType;
+			id = action.payload.id;
+			item = state[itemType].find((m) => m.id === id);
+			item.isDeleting = !item.isDeleting;
+			item.manageMessage = 'Are you sure you want to delete this item?'
+			break;
+		case 'clearItemDeleting':
+			itemType = action.payload.itemType;
+			id = action.payload.id;
+			item = state[itemType].find((m) => m.id === id);
+			item.isDeleting = false;
+			item.manageMessage = 'Manage options:';
+			break;
+		case 'saveItemEditing':
+			itemType = action.payload.itemType;
+			id = action.payload.id;
+			item = state[itemType].find((m) => m.id === id);
+			saveItem = { article, singular, plural } = item;
+			// const response = axios.put(`${api_base_url}/${itemType}/${id}`, saveItem);
+			item.isEditing = false;
+			item.manageMessage = 'Manage options:';
+
+			break;
 	}
 	return obj;
 }
@@ -33,8 +93,16 @@ export const AppProvider = ({ children }) => {
 
 	useEffect(() => {
 		(async () => {
-			const _germanNouns = (await axios.get(`${api_base_url}/germanNouns`)).data;
-			dispatch({type: 'loadGermanNouns', payload: _germanNouns})			
+			const _germanNouns = (
+				await axios.get(`${api_base_url}/germanNouns`)
+			).data;
+			_germanNouns.forEach((m) => {
+				m.isEditing = false;
+				m.isDeleting = false;
+				m.manageMessage = 'Manage options:';
+				m.originalItem = { ...m }
+			});
+			dispatch({ type: 'loadGermanNouns', payload: _germanNouns });
 		})();
 	}, []);
 
